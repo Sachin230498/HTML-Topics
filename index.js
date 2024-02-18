68ba1ca422a5bca2f71890aedb672ff39f6bdf24
Sachin230498 <sachinpathe230498@gmail.com>
2024-02-19 00:29:31 +0530

    html topics

--- index.js
+++
@@ -1,8 +0,0 @@
-function sumDigits(num) {
-  if (num === 0) return 0; // Edge case: if num is 0, return 0
-  return num % 9 || 9; // Calculate the digital root
-}
-
-// Test with the number 12345
-let result = sumDigits(9999);
-console.log("The single digit sum of 12345 is:", result);
